76e77172a824d09ae39974c90d47c3461c241419
ASQAAutomation <meetakhil12@gmail.com>
2024-03-31 20:19:01 +0530

    Test  Message

--- src/main/java/pageObjects/Login/HomePage.java
+++ src/main/java/pageObjects/Login/HomePage.java
@@ -2,11 +2,13 @@ package pageObjects.Login;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 public class HomePage {
 	public WebDriver driver;
+	ChromeDriver driver1 = new ChromeDriver();
 
 	public HomePage(WebDriver driver) {
 		this.driver = driver;
@@ -14,6 +16,7 @@ public class HomePage {
 	}
 
 	@FindBy(xpath = "//div[contains(text(),'Swag Labs')]") WebElement SWAGheader;
+	//driver1.
 	@FindBy(xpath = "//span[contains(text(),'Products')]") WebElement Producttag;
 	@FindBy(id = "add-to-cart-sauce-labs-bike-light") WebElement addtocartbtn;
 	@FindBy(xpath = "//a[@class = 'shopping_cart_link']") WebElement cartlink;
